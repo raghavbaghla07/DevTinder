9514b208fa8eda26dd42bc2a104fbbecdfb63f5f
Raghav Baghla <raghavbaghla.cse@gmail.com>
2026-03-11 20:01:16 +0530

    user connections api

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -3,20 +3,20 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth")
 const ConnectionRequest = require("../models/connectionRequest.js")
 
-
+const USER_SAFE_DATA = "firstName lastName photoUrl"
 //get all the pending connection request for loggedInUser
+
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUserId,
             status: "interested"
-        }).populate("fromUserId", "firstName lastName photoUrl")
+        }).populate("fromUserId", USER_SAFE_DATA)
 
-        return res.json({
-            message: "data fethed successfully",
-            data: connectionRequests
-        })
+        res.json(
+            { data: connectionRequests }
+        )
     } catch (err) {
         return res.status(400)
             .json({
@@ -26,4 +26,34 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     }
 })
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user._id;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+
+            ],
+        })
+            .populate("fromUserId", USER_SAFE_DATA)
+            .populate("toUserId", USER_SAFE_DATA)
+
+        // console.log(connectionRequests)
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId
+            }
+            return row.fromUserId
+        })
+        // console.log(connectionRequests)
+        res.json({ data })
+
+    } catch (err) {
+        return res.status(400)
+            .json({
+                message: err.message
+            })
+    }
+})
 module.exports = userRouter
